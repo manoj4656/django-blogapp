1866070ffb7c30744bb7664d80eaa31a23836ca7
manoj <manoj@example.com>
2024-06-03 23:49:30 +0530

    created posts and created index.html file

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'upload'
+MEDIA_ROOT = BASE_DIR /'upload'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
