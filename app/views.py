14f72b6001b637623a679f9b5caad8e9309066c6
manoj <manoj@example.com>
2024-06-05 21:02:40 +0530

    created posts feature and removed form resubmission issue

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from app.models import Post
+from app.models import Post,Comments
+from app.forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -10,10 +13,24 @@ def index(request):
 
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
+    comments = Comments.objects.filter(post=post)
+    form = CommentForm()
+
+    if request.POST:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid:
+            comment = comment_form.save(commit=False)
+            post_id = request.POST.get('post_id')
+            post = Post.objects.get(id=post_id)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('post_page',kwargs={'slug':slug}))
+
     if post.view_count is None:
         post.view_count = 1
     else:
         post.view_count = post.view_count + 1
     post.save()
-    context = {'post':post}
+
+    context = {'post':post,'form':form,'comments':comments}
     return render(request,'app/post.html',context)
