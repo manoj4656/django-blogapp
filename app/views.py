644cc9e1855d3a37a7e1da0edd8eaf1a00bd33d5
manoj <manoj@example.com>
2024-06-08 01:05:19 +0530

    created post page author page search query function and profile page

--- app/views.py
+++ app/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-from app.models import Post,Comments
-from app.forms import CommentForm
+from app.models import Post,Comments,Tag,Profile
+from app.forms import CommentForm,SubscribeForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db.models import Count
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-    context = {'posts':posts}
+    top_posts = Post.objects.all().order_by('-view_count')[0:3]
+    recent_posts = Post.objects.all().order_by('-last_updated')[0:3]
+    featured_blog = Post.objects.filter(is_featured=True)
+    subscribe_form = SubscribeForm()
+    subscribe_successfull = None
+
+    if featured_blog:
+        featured_blog = featured_blog[0]
+
+    if request.POST:
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid():
+            subscribe_form.save()
+            subscribe_successfull = 'Subscribed successfully'
+            subscribe_form = SubscribeForm()
+
+    context = {'posts':posts,'top_posts':top_posts,'recent_posts':recent_posts,'subscribe_form':subscribe_form,'subscribe_successfull':subscribe_successfull,'featured_blog':featured_blog}
     return render(request,'app/index.html',context)
 
 
@@ -46,3 +64,29 @@ def post_page(request, slug):
 
     context = {'post':post,'form':form,'comments':comments}
     return render(request,'app/post.html',context)
+
+def tag_page(request,slug):
+    tag = Tag.objects.get(slug=slug)
+    top_posts = Post.objects.filter(tag__in=[tag.id]).order_by('-view_count')[0:2]
+    recent_posts = Post.objects.filter(tag__in=[tag.id]).order_by('-last_updated')[0:2]
+
+    tags = Tag.objects.all()
+    context = {'tag':tag,'top_posts':top_posts,'recent_posts':recent_posts,'tags':tags}
+    return render(request,'app/tag.html',context)
+
+def author_page(request,slug):
+    profile = Profile.objects.get(slug=slug)
+    top_posts = Post.objects.filter(author=profile.user).order_by('-view_count')[0:2]
+    recent_posts = Post.objects.filter(author=profile.user).order_by('-last_updated')[0:2]
+    top_authors = User.objects.annotate(number=Count('post')).order_by('number')
+
+    context = {'profile':profile,'top_posts':top_posts,'recent_posts':recent_posts,'top_authors':top_authors}
+    return render(request,'app/author.html',context)
+
+def search_posts(request):
+    search_query = ''
+    if request.GET.get('q'):
+        search_query=request.GET.get('q')
+    posts = Post.objects.filter(title__icontains=search_query)
+    context={'posts':posts,'search_query':search_query}
+    return render(request,'app/search.html',context)
